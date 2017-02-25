bb6f85ccfaf00f7231faf87daa46974332427609
Alex Schuler <alexander.schuler@umconnect.umt.edu>
2017-02-25 16:16:34 -0700

    week 4 upload

---
+++ HW-4/README.md
@@ -0,0 +1,23 @@
+# Alex Schuler Week 4 Assignment
+
+## Musings on Class
+  This week was difficult for me. Most of the language I was using wasn't working how I thought it would. I planned on importing several pre-made images into my piece; but none of my image load commands were doing what I wanted them to, so I made some crappy stand in art.
+
+  I'm struggling with combining a layer-based creation background with all these dependent formulas. It all compounds on each other in a way my brain is having difficulty visualizing. I'm an imagery based learner and when I work on something, I have to be able to be able to visualize the project top-down.
+
+## What We Accomplished
+
+1. Learned new p5 language:
+* Maps function
+* Animating using the Draw Function
+* More math dependent variable functions
+
+## What I Don't Get Yet
+
+1. Too many functions and dependents for me to comfortably work with. I'm becoming less and less enthralled with this type of work. Visual-based tools allow me to focus on my creativity and not be frustrated by cognitive barriers.
+2. Differences between processing and p5, which language is compatible? A lot of my processing commands haven't been working well for me in p5.
+
+### My Sketch
+Homework was inspired by Dylan Belcourt's assignment, saw a way to take it just a bit further: https://marahute.github.io/191-work/HW-3
+
+[p5 Reference Sheet](https://p5js.org/reference/)
---
+++ HW-4/sketch.js
@@ -0,0 +1,134 @@
+//variables
+var bk = 0;
+var r = (108,19,43);
+var fall1 = 0;
+var fall2 = 0;
+var fall3 = 0;
+var fall4 = 0;
+var fall5 = 0;
+var br = (102,51,0);
+
+
+function setup() {
+  createCanvas(600,600);
+}
+
+
+
+function draw() {
+  var currFrameRate;
+  currFrameRate = frameRate();
+  frameRate(60);
+
+  //new background
+  var bgRed, bgGreen, bgBlue;
+
+  // Mapping mouseX to a usable range
+  // bgVal = mouseX;
+  // bgVal = mouseX * 0.425;
+  bgRed = map(mouseX, 0, 600, 0, 255);
+  bgGreen = map(mouseY, 0, 600, 0, 255);
+  bgBlue = map(mouseX + mouseY, 0, 1200, 255, 0);
+
+
+  background(bgRed, bgGreen, bgBlue);
+
+
+/* old background
+  var bgVal;
+
+bgVal = map(mouseX, 0, 600, (240,248,255), (176,224,230));
+
+  background(bgVal);
+*/
+//some identifier values
+  stroke(0);
+  strokeWeight(0);
+
+//Tree log
+pop();
+push();
+fill(102,51,0);
+rect(200,0,150,520);
+pop();
+//tree branch
+push();
+var noiseScale=0.02;
+for (var x=0; x < width; x++) {
+    var noiseVal = noise((mouseX*noiseScale)*noiseScale, mouseY*noiseScale);
+    stroke(noiseVal*255);
+    fill(0,255,0);
+    ellipse(0, 10, 600, 80);
+        ellipse(mouseX+noiseVal*80-25, 10-10, 300, 80);
+            ellipse(mouseX+noiseVal*80-50, 10+20, 300, 80);
+                ellipse(mouseX+noiseVal*80-75, 10-10, 350, 80);
+                    ellipse(mouseX+noiseVal*80-100, 10+20, 300, 80);
+                        ellipse(mouseX+noiseVal*80-125, 10-40, 300, 80);
+                            ellipse(mouseX+noiseVal*80-150, 10+20, 300, 80);
+                                ellipse(mouseX+noiseVal*80-175, 10-40, 250, 80);
+                                    ellipse(mouseX+noiseVal*80-200, 10+30, 250, 80);
+                                        ellipse(mouseX+noiseVal*80-225, 10+20, 250, 80);
+                                            ellipse(mouseX+noiseVal*80-250, 10-10, 250, 80);
+                                                ellipse(mouseX+noiseVal*80-275, 10-20, 250, 80);
+                                                    ellipse(mouseX+noiseVal*80-300, 10+10, 250, 80);
+                                                        ellipse(mouseX+noiseVal*80+25, 10-20, 250, 80);
+                                                            ellipse(mouseX+noiseVal*80+50, 10+30, 250, 80);
+                                                                ellipse(mouseX+noiseVal*80+75, 10-30, 250, 80);
+                                                                    ellipse(mouseX+noiseVal*80+100, 10-20, 250, 80);
+                                                                        ellipse(mouseX+noiseVal*80+125, 10+10, 250, 80);
+                                                                            ellipse(mouseX+noiseVal*80+150, 10-30, 250, 80);
+                                                                                ellipse(mouseX+noiseVal*80+175, 10+20, 400, 80);
+                                                                                    ellipse(mouseX+noiseVal*80+200, 10-10, 300, 80);
+                                                                                        ellipse(mouseX+noiseVal*80+225, 10+40, 300, 80);
+                                                                                            ellipse(mouseX+noiseVal*80+250, 10-20, 300, 80);
+                                                                                                ellipse(mouseX+noiseVal*80+275, 10+10, 300, 80);
+                                                                                                    ellipse(mouseX+noiseVal*80+300, 10-20, 600, 80);
+  }
+
+
+
+//cherries. code from dylan belcourt's snowflakes
+push();
+fill(153,0,0);
+  ellipse(150,fall1,30,30);
+  ellipse(560,fall1,50,50);
+    fall1 = fall1 + 1.2;
+  ellipse(250,fall2,50,50);
+  ellipse(30,fall2,40,40);
+    fall2 = fall2 + 1.9;
+  ellipse(460,fall3,30,30);
+    fall3 = fall3 + 1.9;
+  ellipse(100,fall4,30,30);
+    fall4 = fall4 + 2.2;
+  ellipse(390,fall4,30,30);
+    fall4 = fall4 + 2;
+  ellipse(350,fall5,30,30);
+    fall5 = fall5 + 1;
+pop();
+
+//basket
+  push();
+  fill(102,51,0);
+  rect(mouseX,555,60,50);
+  pop();
+//basket top
+  push();
+  fill(255);
+  rect(mouseX,550,60,3);
+  rect(mouseX,545,60,3);
+  pop();
+  //basket lines
+  push();
+  fill(0);
+  rect(mouseX,560,60,3);
+  rect(mouseX,575,60,3);
+  rect(mouseX,590,60,3);
+  rect(mouseX,560,3,60);
+  rect(mouseX+15,575-15,3,60);
+  rect(mouseX+30, 590-30, 3, 60);
+  rect(mouseX+45, 605-45,3,60);
+  pop();
+
+
+
+}
